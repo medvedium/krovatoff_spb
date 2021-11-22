c4df1c0aa1bc9a6ebfc829a7000c5f95efd34f68
medvedium <mumbles2606@gmail.com>
2021-11-22 21:47:24 +0300

    Updates

---
+++ js/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// import Swiper from 'swiper'
+// import 'swiper/css/bundle'
+var popularSwiper = new Swiper('.popular__slider', {
+  slidesPerView: 4,
+  navigation: {
+    nextEl: '.popular_slider-next',
+    prevEl: '.popular_slider-prev'
+  },
+  pagination: {
+    el: '.popular__slider .swiper-pagination',
+    type: 'bullets'
+  },
+  spaceBetween: 30 // slidesPerColumn: 4
+
+}); // const popularPrev = document.querySelector('')
+
+var premiumSwiper = new Swiper('.premium__slider', {
+  slidesPerView: 4,
+  navigation: {
+    nextEl: '.premium_slider-next',
+    prevEl: '.premium_slider-prev'
+  },
+  pagination: {
+    el: '.premium__slider .swiper-pagination',
+    type: 'bullets'
+  },
+  spaceBetween: 30 // slidesPerColumn: 4
+
+});
