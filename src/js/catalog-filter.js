b6620db1f438501a4b48b78926569405b3f1856c
medvedium <mumbles2606@gmail.com>
2021-12-18 22:46:05 +0300

    Catalog filter styling in progress

--- src/js/catalog-filter.js
+++ src/js/catalog-filter.js
@@ -2,11 +2,36 @@ import noUiSlider from '../../node_modules/nouislider'
 
 if (document.querySelector('#slider')) {
 	noUiSlider.create(document.querySelector('#slider'), {
-		start: [1200, 5000],
+		start: [5000, 350000],
 		connect: true,
 		range: {
-			min: 1000,
-			max: 5000
-		}
+			min: 0,
+			max: 400000
+		},
+		step: 5
+	})
+}
+
+if (document.querySelector('#slider-2')) {
+	noUiSlider.create(document.querySelector('#slider-2'), {
+		start: [100, 220],
+		connect: true,
+		range: {
+			min: 90,
+			max: 250
+		},
+		step: 5
+	})
+}
+
+if (document.querySelector('#slider-3')) {
+	noUiSlider.create(document.querySelector('#slider-3'), {
+		start: [50, 100],
+		connect: true,
+		range: {
+			min: 50,
+			max: 110
+		},
+		step: 5
 	})
 }
